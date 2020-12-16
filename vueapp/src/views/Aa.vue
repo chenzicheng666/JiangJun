5fcdac1e6908b2af7b84b23ea9ed43a3ac04a94d
chenzicheng <chenzicheng_666888>
2020-12-16 21:01:30 +0800

    给  lucy添加Aa.js

---
+++ vueapp/src/views/Aa.vue
@@ -0,0 +1,15 @@
+<template>
+   <div>
+       <h1>Aa文件 hello   lucy</h1>
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Aa"
+    }
+</script>
+
+<style scoped>
+
+</style>
