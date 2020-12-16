5fb9a1d45372be45ba388f4373b6170f0f5d6a2e
chenzicheng <chenzicheng_666888>
2020-12-16 21:03:35 +0800

    ak 提交Aa.vue

---
+++ vueapp/src/views/Aa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Aa文件 hello   ak</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Aa"
+    }
+</script>
+
+<style scoped>
+
+</style>
